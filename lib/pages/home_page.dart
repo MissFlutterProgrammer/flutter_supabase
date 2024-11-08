a9ac1b6f9828605066a511921c40c8a4741269b8
rezaiemehrnegar <rezaiemehrnegar@gmail.com>
2024-11-08 23:32:21 +0330

    11/8/2024 11:32 p.m.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,10 +4,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'create_page.dart';
 import 'edit_page.dart';
 
-
 // Filters
 
-// 1. Equal to : supabase.from('users').select().eq('name','The Rock'); 
+// 1. Equal to : supabase.from('users').select().eq('name','The Rock');
 // 2. Not Equal to : supabase.from('users').select().neq('name','Bill Gates');
 // 3. Greater than : supabase.from('users').select().gt('age',18);
 // 4. Greater than or Equal : supabase.from('users').select().gte('followers',10000);
@@ -21,14 +20,12 @@ import 'edit_page.dart';
 // 1. Order : supabase.from('users').select().order('id',ascending:false);
 // 2. Limit the query : suapbase.from('users').select().limit(10);
 
-
-// RealTime database 
+// RealTime database
 // Syntax : supabase.from('users').stream(primaryKey:['id']).listen((List data){ ..... });
 // Using Streambuilder - stream : supabase.from('users').stream(primaryKey: ['id']);
 
-
 class HomePage extends StatefulWidget {
-   HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -36,18 +33,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final SupabaseClient supabase = Supabase.instance.client;
-  late Stream<List<Map<String,dynamic>>> _readStream;
+  late Stream<List<Map<String, dynamic>>> _readStream;
 
   @override
   void initState() {
-    _readStream = supabase.from('todos').stream(primaryKey: ['id']).eq('user_id', supabase.auth.currentUser!.id).order('id',ascending: false);
+    _readStream = supabase
+        .from('todos')
+        .stream(primaryKey: ['id'])
+        .eq('user_id', supabase.auth.currentUser!.id)
+        .order('id', ascending: false);
     super.initState();
-    
   }
 
   // Syntax to select data
-  Future<List> readData()async{
-    final result = await supabase.from('todos').select().eq('user_id', supabase.auth.currentUser!.id).order('id',ascending: false);
+  Future<List> readData() async {
+    final result = await supabase
+        .from('todos')
+        .select()
+        .eq('user_id', supabase.auth.currentUser!.id)
+        .order('id', ascending: false);
     return result;
   }
 
@@ -57,49 +61,85 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Supabase Flutter"),
         actions: [
-          IconButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>UploadPage()));
-          }, icon: const Icon(Icons.upload_file)),
-          IconButton(onPressed: ()async{
-            await supabase.auth.signOut();
-
-          }, icon: const Icon(Icons.logout))
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UploadPage(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.upload_file),
+          ),
+          IconButton(
+            onPressed: () async {
+              await supabase.auth.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          )
         ],
       ),
-      body:  StreamBuilder(
-        stream: _readStream,
-        builder: (BuildContext context,AsyncSnapshot snapshot){
-          if(snapshot.hasError){
-            return Center(child: Text(snapshot.error.toString()),);
-          }
-
-          if(snapshot.hasData){
-            if(snapshot.data.length == 0){
-              return const Center(child: Text("No data available"),);
+      body: StreamBuilder(
+          stream: _readStream,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error.toString(),
+                ),
+              );
             }
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context,int index){
-                var data = snapshot.data[index]; // {} map
-                
-                  return ListTile(
-                    title: Text(data['title']),
-                    trailing: IconButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>EditPage(data['title'], data['id'])));
-                    }, icon: const Icon(Icons.edit,color: Colors.red,)),
-                  );
-        
-              });
-          }
 
-          return const Center(child: CircularProgressIndicator(),);
-        }),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>CreatePage()));
-          },
-        ),
+            if (snapshot.hasData) {
+              if (snapshot.data.length == 0) {
+                return const Center(
+                  child: Text("No data available"),
+                );
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, int index) {
+                    var data = snapshot.data[index]; // {} map
+
+                    return ListTile(
+                      title: Text(data['title']),
+                      trailing: IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => EditPage(
+                                data['title'],
+                                data['id'],
+                              ),
+                            ),
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.red,
+                        ),
+                      ),
+                    );
+                  });
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CreatePage(),
+            ),
+          );
+        },
+      ),
     );
   }
 }
