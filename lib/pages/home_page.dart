89854ed6bf977368fbc47deb36b4dda2bdc9774b
rezaiemehrnegar <rezaiemehrnegar@gmail.com>
2024-11-08 23:13:27 +0330

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class HomePage extends StatelessWidget {
+   HomePage({super.key});
+  final SupabaseClient supabase = Supabase.instance.client;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: ()async{
+            await supabase.auth.signOut();
+
+          }, icon: const Icon(Icons.logout))
+        ],
+      ),
+      body:  const Center(child:Text("Home Page"))
+    );
+  }
+}
